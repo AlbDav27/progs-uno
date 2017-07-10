a046265a7bfc90e9801f92daceb870c7bfe70377
AlbDav27 <addavlam@gmail.com>
2017-07-10 14:26:44 -0500

    Update rs485.cpp

--- Documents/rs485.cpp
+++ Documents/rs485.cpp
@@ -136,7 +136,7 @@ int main()
 //Este es el bucle que se va a repetir para estar recibiendo constantemente información
 
  
-
+	printf("\n  Llego hasta antes del while \n");
 // se debe de verificar si linux permite que se ejecute indefinidamente el programa, o hay que ejecutar periodicamente el mismo
 
 	while (1)
