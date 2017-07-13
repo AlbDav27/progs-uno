4548e63dec7210c6789cd9a6b9b8a9628537f85b
AlbDav27 <addavlam@gmail.com>
2017-07-13 14:43:21 -0500

    Update p2.c

--- Documents/p2.c
+++ Documents/p2.c
@@ -122,6 +122,7 @@ int main()
 	while (1)
 	{		
 		tcflush(fd, TCIFLUSH);
+		tcflush(fd, TCOFLUSH);
 		printf("\n\n n= %i  f = %i    \n\n", n , f);
 		//////////////////////Cada ciclo completo de medición de todas las fuentes y controladores, incluye el envio de información///////
 		if(f<=6)
