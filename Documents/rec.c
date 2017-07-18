efe4ca67cb4db9f4174d12b92930a435b09e88fd
AlbDav27 <addavlam@gmail.com>
2017-07-18 12:54:24 -0500

    Update rec.c

--- Documents/rec.c
+++ Documents/rec.c
@@ -123,8 +123,10 @@ int main()
 
 
 		usleep(100000);
+
+		n=serial_read(fd,read_buffer,8,1000000);
 		//printf("\nantes de leer");
-		bytes_read = read(fd,read_buffer, 8); /* Read the data                   */
+		
 		
 
         printf("\n Recibo:   ");
@@ -144,3 +146,48 @@ int main()
 	
      close(fd); /* Close the serial port */
 }
+
+int serial_read(int serial_fd, char *data, int size, int timeout_usec)
+{
+  fd_set fds;
+  struct timeval timeout;
+  int count=0;
+  int ret;
+  int n;
+  
+  //-- Wait for the data. A block of size bytes is expected to arrive
+  //-- within the timeout_usec time. This block can be received as 
+  //-- smaller blocks.
+  do {
+      //-- Set the fds variable to wait for the serial descriptor
+      FD_ZERO(&fds);
+      FD_SET (serial_fd, &fds);
+    
+      //-- Set the timeout in usec.
+      timeout.tv_sec = 0;  
+      timeout.tv_usec = timeout_usec;
+
+      //-- Wait for the data
+      ret=select (FD_SETSIZE,&fds, NULL, NULL,&timeout);
+    
+    //-- If there are data waiting: read it
+      if (ret==1) {
+        
+        //-- Read the data (n bytes)
+        n=read (serial_fd, &data[count], size-count); 
+        
+        //-- The number of bytes receives is increased in n
+        count+=n;
+        
+        //-- The last byte is always a 0 (for printing the string data)
+        data[count]=0;
+      }
+
+    //-- Repeat the loop until a data block of size bytes is received or
+    //-- a timeout occurs
+  } while (count<size && ret==1);
+
+  //-- Return the number of bytes reads. 0 If a timeout has occurred.
+  return count;
+}
+
