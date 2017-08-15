415e921ab6e3e16e0f92fad3e33e41ecc68927d6
AlbDav27 <addavlam@gmail.com>
2017-08-15 17:14:22 -0500

    Create e.c

---
+++ Documents/e.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>   /* ioctl() */
+#include <unistd.h> 	//para funcion sleep()  ...delay
+#include <string.h>
+
+int main(){
+	char com[100];
+	char lin[100];
+	char res[100];
+	char caracter;
+	int n=0;
+	FILE *fp;
+
+	//system ("/usr/advantech/utility/3g-module/3g_ublox.sh >/home/root/progs-uno/Documents/cnf.txt");
+
+	fp = fopen ("cnf.txt","r");
+
+	printf("\n");
+	while (n<999){
+		caracter = fgetc(fp);		
+		printf("n= %d , %c  //", n, caracter);
+		n++;
+	}
+	fclose(fp);
+	printf("\nLa cadena de DNS: %s\n", res);
+}
