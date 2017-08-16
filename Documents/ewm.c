7e2dc388e7e40a3f64501934195da81cf5f7902b
AlbDav27 <addavlam@gmail.com>
2017-08-15 19:07:33 -0500

    Update ewm.c

--- Documents/ewm.c
+++ Documents/ewm.c
@@ -6,20 +6,38 @@
 
 int main(){
 	char com[100];
-	char lin[100];
-	char res[100];
-	char caracter;
+	char lin[50];
+	char ipd[20];
+	char c;
 	int n=0;
+	itn x=0;
+	int lt;
 	FILE *fp;
 
 	system ("/usr/advantech/utility/3g-module/3g_ublox.sh >/home/root/progs-uno/Documents/cnf.txt");
 	sleep(7);
+	strcpy(com, "");
+	strcpy(lin, "");
+	strcpy(ipd, "");
 	system ("sed '1,41d' /home/root/progs-uno/Documents/cnf.txt >/home/root/progs-uno/Documents/cn.txt");
 
 	fp = fopen ("cn.txt","r");
 
 	printf("\n");
 	fgets(lin, 100, fp);
-		printf(" %s  //", lin);
+	printf(" %s  //", lin);
 	fclose(fp);
+	lt = strlen(lin);
+	printf("\n la longitud de la cadena completa es: %d\n", lt);
+	while (n<lt){
+		c=lin[n];
+		if (c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'||c=='.'){
+			ipd[x]=c;
+			x++;
+		}
+		n++;
+	}
+	lt = strlen(ipd);
+	printf("\n la dir ip del ser dns es: %s\n", ipd);
+	printf("\n la long de la dir ip: %d\n", lt);
 }
