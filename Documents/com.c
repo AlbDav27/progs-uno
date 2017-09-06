61c66102946c143ac3531c242c5e6919a1ece168
AlbDav27 <addavlam@gmail.com>
2017-09-06 12:13:15 -0500

    Update com.c

--- Documents/com.c
+++ Documents/com.c
@@ -72,7 +72,7 @@ int main(){
 		bytes_written = write(fd,cmx,8);
 		//ioctl(fd,TIOCMBIS,&RTS_flag);                       
 		//ioctl(fd,TIOCMBIS,&DTR_flag);
-		usleep(3000000);	
+		//usleep(3000000);	
 		bytes_read = read(fd,read_buffer,35);		//leer datos y almacenarlos en el array read_buffer
         //ioctl(fd,TIOCMBIC,&RTS_flag); 
         printf("\n Recibo: /");
