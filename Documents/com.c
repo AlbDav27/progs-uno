39785956f54edc3b8ee97c70a251682b4a35c9f6
AlbDav27 <addavlam@gmail.com>
2017-09-06 11:54:56 -0500

    Create com.c

---
+++ Documents/com.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>             /* File Control Definitions           */
+#include <termios.h>      /* POSIX Terminal Control Definitions */
+#include <unistd.h>         /* UNIX Standard Definitions            */
+#include <errno.h>           /* ERROR Number Definitions           */
+#include <sys/ioctl.h>   /* ioctl() */
+#include <unistd.h> 	//para funcion sleep()  ...delay
+#include <string.h>
+
+int main(){
+	char cmx[8];
+	char read_buffer[35];
+	int bytes_read;
+	int bytes_written;
+
+	int fd;/*File Descriptor*/
+	/*------------------------------- Opening the Serial Port -------------------------------*/
+	/* Change /dev/ttyS0 to the one corresponding to your system */
+	fd = open("/dev/ttyS0",O_RDWR | O_NOCTTY | O_NDELAY);           /* ttyS0 corresponde al puerto serial rs485 de UNO-1252G   */  
+                                                              /* O_RDWR   - Read/Write access to serial port       */
+                                                              /* O_NOCTTY - No terminal will control the process   */
+                                                              /* Open in blocking mode,read will wait              */
+	///////SI CON EL PUERTO ABIERTO NORMALMENTE SIGUE SIN RESPETARSE LAS CONDICIONES VMIN Y VTIME, SE DEBE AGREGAR A LA INSTRUCCION OPEN | O_NDELAY
+	if(fd == -1){          
+		printf("\n  Error! in Opening ttyS0  ");
+	}
+	else{
+		printf("\n  ttyS0 Opened Successfully ");
+	}
+	/*---------- Setting the Attributes of the serial port using termios structure --------- */
+	struct termios SerialPortSettings;	/* Create the structure                          */
+	tcgetattr(fd, &SerialPortSettings);	/* Get the current attributes of the Serial port */
+		/* Setting the Baud rate */
+	cfsetispeed(&SerialPortSettings,B9600); /* Set Read  Speed as 9600                       */
+	cfsetospeed(&SerialPortSettings,B9600); /* Set Write Speed as 9600                       */
+		/* 8N1 Mode */
+	SerialPortSettings.c_cflag &= ~PARENB;   /* Disables the Parity Enable bit(PARENB),So No Parity   */
+	SerialPortSettings.c_cflag &= ~CSTOPB;   /* CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit */
+	SerialPortSettings.c_cflag &= ~CSIZE;	 /* Clears the mask for setting the data size             */
+	SerialPortSettings.c_cflag |=  CS8;      /* Set the data bits = 8                                 */
+	SerialPortSettings.c_cflag &= ~CRTSCTS;       /* No Hardware flow Control                         */
+	SerialPortSettings.c_cflag |= CREAD | CLOCAL; /* Enable receiver,Ignore Modem Control lines       */ 
+	SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          /* Disable XON/XOFF flow control both i/p and o/p */
+	SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHOE | ISIG);  /* Non Cannonical mode                            */
+	SerialPortSettings.c_oflag &= ~OPOST;/*No Output Processing*/
+		/* Setting Time outs */
+	SerialPortSettings.c_cc[VMIN] =  0; 		//pure time read 
+	SerialPortSettings.c_cc[VTIME] = 10;  /* Wait 1s   */	//VTIME TIEMPO EN DECIMAS DE SEGUNDO
+	/*
+		int RTS_flag,DTR_flag;
+
+		RTS_flag = TIOCM_RTS;	 Modem Constant for RTS pin 
+		DTR_flag = TIOCM_DTR;	 Modem Constant for DTR pin 
+
+		ioctl(fd,TIOCMBIC,&RTS_flag); ~RTS = 1,So ~RE pin of MAX485 is HIGH                       
+		ioctl(fd,TIOCMBIC,&DTR_flag); ~DTR = 1,So  DE pin of MAX485 is HIGH,Transmit Mode enabled 
+ 
+	*/
+	//fcntl(fd, F_SETFL,FNDELAY);
+
+	if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0){ 
+		    printf("\n  ERROR ! in Setting attributes");
+	}
+	else{
+             printf("\n  BaudRate = 9600 \n  StopBits = 1 \n  Parity   = none");
+ 	}
+ 	while (1){
+ 		strcpy(cmx, "cr02/***");
+		printf("\nSolicitud: %s/findecad", cmx);
+		bytes_written = write(fd,cmx,8);
+		ioctl(fd,TIOCMBIS,&RTS_flag);                       
+		//ioctl(fd,TIOCMBIS,&DTR_flag);
+		usleep(900000);	
+		bytes_read = read(fd,read_buffer,34);		//leer datos y almacenarlos en el array read_buffer
+        ioctl(fd,TIOCMBIC,&RTS_flag); 
+        printf("\n Recibo: /");
+        for(i=0;i<bytes_read;i++)              /*printing only the received characters*/
+			printf("%c",read_buffer[i]);
+		printf("/fincadena \n +----------------------------------+\n\n");
+		bytes_read=0;
+ 	}
+ 	
